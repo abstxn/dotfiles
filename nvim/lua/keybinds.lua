29d58ab52116632aa6853f7e750942918300a371
abstxn <108669111+abstxn@users.noreply.github.com>
2022-12-10 15:51:17 +0800

    Remove "hp" as hop keybind for nvim

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -1,3 +1,3 @@
 vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'hp', ':HopWord<CR>', { noremap = true })
+-- vim.api.nvim_set_keymap('n', 'hp', ':HopWord<CR>', { noremap = true })
 
